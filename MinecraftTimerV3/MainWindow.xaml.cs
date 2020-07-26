c2abb6a7e5010ebf11adf8642d0cc76c67381c1e
filipton12 <37213766+filipton12@users.noreply.github.com>
2020-07-26 11:50:28 +0200

    1.1
    -Added fully changable background and text color

--- MinecraftTimerV3/MainWindow.xaml.cs
+++ MinecraftTimerV3/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,12 +18,38 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Path = System.IO.Path;
+
+[Serializable]
+public struct FColors
+{
+	public FColor BackgroundColor;
+	public FColor TextColor;
+
+	public FColors(FColor backColor, FColor txtColor)
+	{
+		BackgroundColor = backColor;
+		TextColor = txtColor;
+	}
+}
+
+[Serializable]
+public struct FColor
+{
+	public byte r;
+	public byte g;
+	public byte b;
+
+	public FColor(byte R, byte G, byte B)
+	{
+		r = R;
+		g = G;
+		b = B;
+	}
+}
 
 namespace MinecraftTimerV3
 {
-	/// <summary>
-	/// Logika interakcji dla klasy MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public long time = 0;
@@ -45,6 +72,24 @@ namespace MinecraftTimerV3
 
 			Width = 190;
 			Height = 170;
+
+			string jsonPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "colors.json");
+			if (!File.Exists(jsonPath))
+			{
+				FColors tmpfcolors = new FColors(new FColor(23, 154, 41), new FColor(255, 255, 255));
+				string json = JsonConvert.SerializeObject(tmpfcolors);
+
+				File.WriteAllText(jsonPath, json);
+			}
+
+			FColors fcolors = JsonConvert.DeserializeObject<FColors>(File.ReadAllText(jsonPath));
+
+			Background = new SolidColorBrush(Color.FromRgb(fcolors.BackgroundColor.r, fcolors.BackgroundColor.g, fcolors.BackgroundColor.b));
+			SolidColorBrush sob = new SolidColorBrush(Color.FromRgb(fcolors.TextColor.r, fcolors.TextColor.g, fcolors.TextColor.b));
+			rtl.Foreground = sob;
+			rtl_tb.Foreground = sob;
+			gt.Foreground = sob;
+			gt_tb.Foreground = sob;
 		}
 
 		void timer_Tick(object sender, EventArgs e)
